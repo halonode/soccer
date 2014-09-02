debad00e94257bd503687ca7d14c915ec41526fa
Haluk Demir <web@halukdemir.com>
2014-09-02 21:27:29 +0300

    origin

--- app/routes.php
+++ app/routes.php
@@ -1,5 +1,5 @@
 <?php
-header('Access-Control-Allow-Origin: http://www.halukdemir.com');
+header('Access-Control-Allow-Origin: *');
 /*
 |--------------------------------------------------------------------------
 | Application Routes
